57f70b79453723a5865d935dbed8b0b6554d742f
Lynch Mwaniki <lsking16@gmail.com>
2019-03-02 04:14:21 +0200

    Added new route to about webpage

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+
+// Route to about page:
+app.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
 app.listen(3000)
 console.log('Express server running on port 3000')
